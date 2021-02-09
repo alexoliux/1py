f7ee3828148bba0e8dc947669ff34fb95bb6c989
unknown <ivcbibl@VLAN-25-BI-081.rd.mirea.ru>
2021-02-09 15:03:08 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+
+import math
+from math import *
+
+if __name__ == '__main__':
+    x, y, z = 65, 39, 12
+    f1 = sqrt(pow(z, 3)+cos(x))
+    f2 = (cos(y)+cos(y))/(fabs(x)-cos(x))
+    f3 = (exp(y)+cos(y)-51)/(tan(x)+96*pow(z, 5))
+    f = f1-f2-f3
+    print '%e'  %f
